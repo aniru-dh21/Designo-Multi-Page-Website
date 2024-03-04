8640d5264c3c9910e722d646ae5c6066147f09db
Ramachandra Anirudh Vemulapalli <anirudh@Ramachandras-MacBook-Air.local>
2024-03-04 19:05:16 +0530

    Changes made

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PageContainer from "./components/shared/page.container";
+import PageContent from "./components/shared/page.content";
+import Header from "./components/header/header";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <PageContainer>
+        <PageContent>
+          <Header />
+        </PageContent>
+      </PageContainer>
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div>Hello, World!</div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
